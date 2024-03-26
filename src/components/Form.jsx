6198f75e36f5972b6d91d861b50d29ebf6452a5f
David Bausela <dbausela@gmail.com>
2024-03-26 12:46:52 +0100

    Paso: Handling form submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,12 @@
 function Form() {
+
+    function handleSubmit(event) {
+      event.preventDefault();
+      alert("Hello, world!");
+    }
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
