b7066cd23c48b78972da5150edc8799a18112f1d
David Bausela <dbausela@gmail.com>
2024-03-26 12:03:21 +0100

    Paso: propiedad id

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,9 @@ function App(props) {
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-        <Todo name="Eat" completed />
-        <Todo name="Sleep" />
-        <Todo name="Repeat" />
+        <Todo name="Eat" id="todo-0" completed />
+        <Todo name="Sleep" id="todo-1" />
+        <Todo name="Repeat" id="todo-2" />
       </ul>
     </div>
   );
